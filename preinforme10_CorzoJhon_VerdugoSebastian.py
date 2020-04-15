59d632561383269537634b2f6536caae98d85113
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-04-14 21:26:56 -0500

    Parte a, respuesta sexto y septimo ejercicio.

--- preinforme10_CorzoJhon_VerdugoSebastian.py
+++ preinforme10_CorzoJhon_VerdugoSebastian.py
@@ -19,7 +19,22 @@ print("La media de la utilidad operacional durante los años de registro es:",np
 print(a)
 
 #Algoritmo quinta pregunta
-acum=0
 for i in range(0,10):
-    por=(uti_op_kellogs[i]*100)/ np.sum(uti_op_kellogs)
-    print("El porcentaje que aporta la utilidad del año",i+1,"evaluado, es de",por,"%")
+    total = np.sum(uti_op_kellogs)
+    por=(uti_op_kellogs[i]*100)/ total
+    print("El porcentaje que aporta la utilidad del año",i+1,"evaluado, es de",por,"%.")
+
+#Algoritmo sexta pregunta
+defic = uti_op_kellogs[9:0:-10] - uti_op_kellogs[8:9]
+print("El déficit en la utilidad operacional del año 2017 con respecto a la del año pasado fue de",defic,"millones de COP.")
+
+#Algoritmo septima pregunta
+for x in range(0,10):
+    if x==0:
+        rest = uti_op_kellogs[x]
+        print("En el primer año no existe déficit.")
+    else:
+        defic = rest - uti_op_kellogs[x]
+        rest = uti_op_kellogs[x]
+        por = defic/ total
+        print("El porcentaje de déficit del año",i+1,"con respecto al año posterior es de",por,"%.")
