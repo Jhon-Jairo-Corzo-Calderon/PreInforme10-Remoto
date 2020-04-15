444c2d1838e9b478fd3c046f621749b28016be48
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-04-14 19:10:11 -0500

    Parte a, respuesta tercer,cuarto y quinto ejercicio.

--- preinforme10_CorzoJhon_VerdugoSebastian.py
+++ preinforme10_CorzoJhon_VerdugoSebastian.py
@@ -4,12 +4,22 @@ uti_op_kellogs = np.array([27834, 23789, 30189, 30967, 32501, 32701, 31665, 1715
 
 #Algoritmo primera pregunta
 dif_pro = np.mean(uti_op_kellogs[8:10]) - np.mean(uti_op_kellogs[:2])
-
 print("El resultado de la diferencia del promedio de los 2 ultimos años, con los dos primeros años es:",dif_pro)
 
 #Algoritmo segunda pregunta
 dif_maxmin = np.max(uti_op_kellogs) - np.min(uti_op_kellogs)
-
 print("El resultado de la diferencia entre las utilidades operacionales del año con mayor utilidad y el año con menor utilidad es:",dif_maxmin)
 
+#Algoritmo tercera pregunta
+a = print("La mediana de la utilidad operacional durante los años de registro es:",np.median(uti_op_kellogs))
+print(a)
 
+#Algoritmo cuarta pregunta
+print("La media de la utilidad operacional durante los años de registro es:",np.mean(uti_op_kellogs))
+print(a)
+
+#Algoritmo quinta pregunta
+acum=0
+for i in range(0,10):
+    por=(uti_op_kellogs[i]*100)/ np.sum(uti_op_kellogs)
+    print("El porcentaje que aporta la utilidad del año",i+1,"evaluado, es de",por,"%")
